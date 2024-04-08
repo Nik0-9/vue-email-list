6844d2983d63f4c33749050f8b04ed9267fea484
Nik0-9 <filannino.nicola9@gmail.com>
2024-04-08 17:58:50 +0200

    ADD control in getEmailList of length of array for fix error print less 10 element in array

--- js/myscript.js
+++ js/myscript.js
@@ -3,27 +3,27 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            emails:[],
+            emails: [],
             emailNum: 10,
             apiPath: 'https://flynn.boolean.careers/exercises/api/',
-        }
+        };
     },
     methods:{
         getEmailList(){
             const newList = [];
-            for(let i = 1; i <= this.emailNum; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+            for(let i = 0; i < this.emailNum; i++){
+                axios.get(`${this.apiPath}random/mail`).then((res)=>{
                     const mail = res.data.response;
                     newList.push(mail);
-                    if(i === this.emailNum){
+                    
+                    if(newList.length === this.emailNum ){
                         this.emails = [...newList];
                     }
                 });
             }
         }
-
     },
     created(){
-        this.getEmailList();
+       this.getEmailList();
     }
 }).mount('#app')
