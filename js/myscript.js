15f8ad565c04a30b0067d939c54c626877b18955
Nik0-9 <filannino.nicola9@gmail.com>
2024-04-08 14:44:26 +0200

    something isn't working

--- js/myscript.js
+++ js/myscript.js
@@ -3,11 +3,26 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
+            emails:[],
+            emailNum: 10,
+            apiPath: 'https://flynn.boolean.careers/exercises/api/',
         }
     },
     methods:{
-
+        getEmailList(){
+            const newList = [];
+            for(let i = 0; i < emailNum; i++){
+                axios.get(`${this.apiPath}random/mail`).then((res)=>{
+                    const mail = res.data.response;
+                    newList.push(mail);
+                    
+                    this.emails = [...newList];
+                });
+            }
+        }
 
     },
+    created(){
+        this.getEmailList();
+    }
 }).mount('app')
